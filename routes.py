d54bf0a8f2a176e4ffd9ca19d44f61987c5cd2f3
Marcin Andrzejczak <wtpromise@gmail.com>
2023-01-07 04:25:11 +0100

    form

--- routes.py
+++ routes.py
@@ -1,8 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 page = Blueprint('page', __name__, static_folder='static', template_folder='templates')
 
 
-@page.route('/')
+@page.route('/', methods=['GET', 'POST'])
 def index():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+    data = {
+        'name': name,
+        'email': email,
+        'message': message
+    }
+    print(data)
     return render_template('index.html')
