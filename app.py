2ef7c7eba0ec47f2d0268c688223269373e91eec
Marcin Andrzejczak <wtpromise@gmail.com>
2023-01-09 19:11:16 +0100

    asdf

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+import os
+
 from routes import page
 from flask import Flask
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 load_dotenv()
 
 
 def create_app():
     app = Flask('__name__')
+    client = MongoClient(os.environ.get('MONGO_DB'))
+    app.db = client.get_default_database()
     app.register_blueprint(page)
     return app
 
