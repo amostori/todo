6acb0ac02fa53f901a115252ddb2828b51c015ad
Marcin Andrzejczak <wtpromise@gmail.com>
2023-01-10 16:04:30 +0100

    message only

--- routes.py
+++ routes.py
@@ -6,13 +6,9 @@ page = Blueprint('page', __name__, static_folder='static', template_folder='temp
 @page.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
         message = request.form.get('message')
-        current_app.db.entries.insert_one({'name': name,
-                                           'email': email,
-                                           'message': message})
-    entries = [(entry['message'], entry['email'], entry['name']) for entry in
+        current_app.db.entries.insert_one({'message': message})
+    entries = [entry['message'] for entry in
                current_app.db.entries.find({})]
     all_test = current_app.db.entries.find({})
     return render_template('index.html', entries=entries, all_test=all_test)
