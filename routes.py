1a44d8b98e83c95c7dfc640137e01590117d42be
Marcin Andrzejczak <wtpromise@gmail.com>
2023-01-13 12:09:09 +0100

    working

--- routes.py
+++ routes.py
@@ -5,17 +5,23 @@ page = Blueprint('page', __name__, static_folder='static', template_folder='temp
 
 @page.route('/', methods=['GET'])
 def index():
-    messages = [entry['message'] for entry in
-                current_app.db.entries.find({})]
-    all_test = current_app.db.entries.find({})
+    messages = [(entry['name'], entry['message']) for entry in
+                current_app.db.poems.find({})]
     return render_template('index.html', messages=messages, all_test=all_test)
 
 
 @page.route('/submit_message', methods=['POST'])
 def submit_message():
+    name = request.form.get('name')
     message = request.form.get('message')
-    current_app.db.entries.insert_one({'message': message})
-    response = f"""
-    <li>{message}</li>
-    """
+    current_app.db.poems.insert_one({'message': message, 'name': name})
+    if name != '':
+        response = f"""
+        <h4>{name}</h4>
+        <p>{message}</p>
+        """
+    else:
+        response = f"""
+                <p>{message}</p>
+                """
     return response
